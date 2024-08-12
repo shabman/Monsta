7f67b5a9540cd80b3c3d16d8d3ae8bc2948805b3
shabman <avia.shabbyman@gmail.com>
2024-08-13 00:37:09 +0100

    Create Context API

---
+++ include/Monsta/Renderer/Context/Context.h
@@ -0,0 +1,38 @@
+/**
+ *  Monsta 3D Game Engine Framework.
+ *  Copyright (C) 2024 Mustafa Malik (avia.shabbyman@gmail.com)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIB_MONSTA_CONTEXT_H
+#define LIB_MONSTA_CONTEXT_H
+
+namespace Monsta::Renderer
+{
+
+class Context
+{
+public:
+  Context ();
+  virtual ~Context () = 0;
+
+public:
+  virtual void init () noexcept = 0;
+  virtual void run () noexcept = 0;
+};
+
+}
+
+#endif /* LIB_MONSTA_CONTEXT_H */
