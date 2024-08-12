e8fb815eef42c71589b2f89ca12c2097b79882d0
shabman <avia.shabbyman@gmail.com>
2024-08-13 00:38:22 +0100

    Using Window API

--- src/main.cpp
+++ src/main.cpp
@@ -6,4 +6,12 @@ main ()
 {
   std::cout << "Hello Monsta 3D!\n";
   std::cout << __cplusplus << "\n";
+
+  Monsta::Core::Window window ( 900, 500, "Test" );
+  Monsta::Core::Window window2 ( 800, 400, "Test2" );
+
+  window.show ();
+  window2.show ( true );
+
+  window.start ();
 }
